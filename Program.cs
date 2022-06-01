aee13e9a48e8dbab7af127a0562a826caa695bf1
edmilson17 <edmilsonribeiro.17072005@gmail.com>
2022-05-31 22:27:32 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿    
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("inicio: ");
+        int inicio = Convert.ToInt32(Console.ReadLine());
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("fim: ");
+        int fim = Convert.ToInt32(Console.ReadLine());
+        Console.ResetColor();
+
+        if (fim >= inicio)
+
+        {
+            int n = inicio;
+
+            while (n <= fim)
+            {
+                Console.Write($"{n} ");
+                n += 1;
+            }
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Não é possivel terminar antes de iniciar. ");
+            Console.ResetColor();
+        }
+    }
+
+
+
+
+
